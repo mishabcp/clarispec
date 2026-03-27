b533f9606cc6047052e68ca889e51871bc258b16
Mishab CP <mishabcp01@gmail.com>
2026-03-28 04:19:28 +0530

    chore(login): client page console log on mount

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,12 @@
+'use client'
+
+import { useEffect } from 'react'
 import { LoginForm } from '@/components/auth/LoginForm'
 
 export default function LoginPage() {
+  useEffect(() => {
+    console.log('we are now in login page')
+  }, [])
+
   return <LoginForm />
 }
